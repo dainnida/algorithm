ebb18c61498b7dd8fd9b552ec536c1988a022409
다인 <129330821+dainnida@users.noreply.github.com>
2026-06-25 00:29:12 +0900

    [Implement] 삽입 정렬

---
+++ InsertSort.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+public class InsertSort {
+    // 시간복잡도: 최악 O(n^2), 최선 O(n)
+    // 공간복잡도: O(n)
+    public static void sort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int temp = arr[i];
+            int index = i - 1;
+            while (index >= 0 && arr[index] > temp) {
+                arr[index + 1] = arr[index];
+                index--;
+            }
+            arr[index + 1] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[] {29, 10, 27, 49, 26, 43, 23, 32, 5};
+        System.out.println(Arrays.toString(arr));
+        InsertSort.sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
